459c5366e753bc070328f742b74bc8c66d9faeb8
Ghadah AlMuhaideb <54583470+Griymu@users.noreply.github.com>
2023-07-07 18:44:34 +0300

    Delete lib directory

--- lib/resources/constant_maneger.dart
+++
@@ -1,7 +0,0 @@
-class AppConstant {
-  static const splashDelay = 2;
-  static const sliderAnimation = 300;
-  static const timerDuration = Duration(minutes: 15);
-  static const constPhoneNumber = "+966504801805";
-  static const String parkingAreasCollectionName = 'parking-areas';
-}
